7bbc9a7d5064af85de3fc691b2190ffa08436ef3
T. Tran <ttran375@my.centennialcollege.ca>
2024-04-13 12:20:10 +0000

    Sat, Apr 13, 2024, 12:20 PM +00:00

--- create.sql
+++ create.sql
@@ -1,134 +1,134 @@
 CREATE TABLE gpnr_category (
-  category_id NUMBER(10) NOT NULL PRIMARY KEY,
-  category_name VARCHAR2(255) NOT NULL,
-  category_description CLOB,
-  parent_category_id NUMBER(10),
-  CONSTRAINT fk_parent_category_id FOREIGN KEY (parent_category_id) REFERENCES gpnr_category(category_id)
+    category_id NUMBER(10) NOT NULL PRIMARY KEY,
+    category_name VARCHAR2(255) NOT NULL,
+    category_description CLOB,
+    parent_category_id NUMBER(10),
+    FOREIGN KEY (parent_category_id) REFERENCES gpnr_category(category_id)
 );
 
 CREATE TABLE gpnr_payment_info (
-  payment_info_id NUMBER(10) NOT NULL PRIMARY KEY,
-  payment_info_name VARCHAR2(255) NOT NULL,
-  card_number VARCHAR2(19) NOT NULL,
-  card_name VARCHAR2(255) NOT NULL,
-  card_expiration DATE NOT NULL,
-  card_cvv CHAR(4) NOT NULL
+    payment_info_id NUMBER(10) NOT NULL PRIMARY KEY,
+    payment_info_name VARCHAR2(255) NOT NULL,
+    card_number VARCHAR2(19) NOT NULL,
+    card_name VARCHAR2(255) NOT NULL,
+    card_expiration DATE NOT NULL,
+    card_cvv CHAR(4) NOT NULL
 );
 
 CREATE TABLE gpnr_customer (
-  customer_username VARCHAR2(255) NOT NULL PRIMARY KEY,
-  customer_password VARCHAR2(255) NOT NULL,
-  customer_email VARCHAR2(255) NOT NULL,
-  customer_firstname VARCHAR2(255) NOT NULL,
-  customer_lastname VARCHAR2(255) NOT NULL,
-  customer_address CLOB,
-  customer_phone VARCHAR2(20),
-  payment_info_id NUMBER(10),
-  CONSTRAINT fk_payment_info_id FOREIGN KEY (payment_info_id) REFERENCES gpnr_payment_info(payment_info_id)
+    customer_username VARCHAR2(255) NOT NULL PRIMARY KEY,
+    customer_password VARCHAR2(255) NOT NULL,
+    customer_email VARCHAR2(255) NOT NULL,
+    customer_firstname VARCHAR2(255) NOT NULL,
+    customer_lastname VARCHAR2(255) NOT NULL,
+    customer_address CLOB,
+    customer_phone VARCHAR2(20),
+    payment_info_id NUMBER(10),
+    FOREIGN KEY (payment_info_id) REFERENCES gpnr_payment_info(payment_info_id)
 );
 
 CREATE TABLE gpnr_product_attribute (
-  product_attribute_id NUMBER(10) NOT NULL PRIMARY KEY,
-  product_attribute_name VARCHAR2(255) NOT NULL,
-  product_attribute_status VARCHAR2(9) NOT NULL CHECK (product_attribute_status IN ('active', 'inactive')),
-  value CLOB
+    product_attribute_id NUMBER(10) NOT NULL PRIMARY KEY,
+    product_attribute_name VARCHAR2(255) NOT NULL,
+    product_attribute_status VARCHAR2(10) CHECK(product_attribute_status IN ('active', 'inactive')) NOT NULL DEFAULT 'active',
+    value CLOB
 );
 
-CREATE TABLE gpnr_product (
-  product_id NUMBER(10) NOT NULL PRIMARY KEY,
-  product_name VARCHAR2(255) NOT NULL,
-  product_description CLOB,
-  product_created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
-  product_status VARCHAR2(13) NOT NULL CHECK (product_status IN ('available', 'out_of_stock', 'discontinued')),
-  price NUMBER(10, 2) NOT NULL,
-  currency CHAR(3) NOT NULL,
-  stock NUMBER(10) NOT NULL DEFAULT 0,
-  category_id NUMBER(10),
-  product_attribute_id NUMBER(10),
-  CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES gpnr_category(category_id),
-  CONSTRAINT fk_product_attribute_id FOREIGN KEY (product_attribute_id) REFERENCES gpnr_product_attribute(product_attribute_id)
+CREATE TABLE GPNR_PRODUCT (
+    PRODUCT_ID NUMBER(10) NOT NULL PRIMARY KEY,
+    PRODUCT_NAME VARCHAR2(255) NOT NULL,
+    PRODUCT_DESCRIPTION CLOB,
+    PRODUCT_CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRODUCT_STATUS VARCHAR2(15) CHECK(PRODUCT_STATUS IN ('available', 'out_of_stock', 'discontinued')) NOT NULL DEFAULT 'available',
+    PRICE DECIMAL(10, 2) NOT NULL,
+    CURRENCY CHAR(3) NOT NULL,
+    STOCK NUMBER(10) NOT NULL DEFAULT 0,
+    CATEGORY_ID NUMBER(10),
+    PRODUCT_ATTRIBUTE_ID NUMBER(10),
+    FOREIGN KEY (CATEGORY_ID) REFERENCES GPNR_CATEGORY(CATEGORY_ID),
+    FOREIGN KEY (PRODUCT_ATTRIBUTE_ID) REFERENCES GPNR_PRODUCT_ATTRIBUTE(PRODUCT_ATTRIBUTE_ID)
 );
 
-CREATE TABLE gpnr_request (
-  request_id NUMBER(10) NOT NULL PRIMARY KEY,
-  request_cost NUMBER(10, 2) NOT NULL,
-  request_quantity NUMBER(10) NOT NULL DEFAULT 1,
-  request_created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
-  request_status VARCHAR2(10) NOT NULL CHECK (request_status IN ('pending', 'processing', 'completed', 'cancelled')),
-  product_quantity NUMBER(10) NOT NULL DEFAULT 1,
-  message CLOB,
-  customer_username VARCHAR2(255),
-  product_id NUMBER(10),
-  CONSTRAINT fk_customer_username FOREIGN KEY (customer_username) REFERENCES gpnr_customer(customer_username),
-  CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES gpnr_product(product_id)
+CREATE TABLE GPNR_REQUEST (
+    REQUEST_ID NUMBER(10) NOT NULL PRIMARY KEY,
+    REQUEST_COST DECIMAL(10, 2) NOT NULL,
+    REQUEST_QUANTITY NUMBER(10) NOT NULL DEFAULT 1,
+    REQUEST_CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    REQUEST_STATUS VARCHAR2(15) CHECK(REQUEST_STATUS IN ('pending', 'processing', 'completed', 'cancelled')) NOT NULL DEFAULT 'pending',
+    PRODUCT_QUANTITY NUMBER(10) NOT NULL DEFAULT 1,
+    MESSAGE CLOB,
+    CUSTOMER_USERNAME VARCHAR2(255),
+    PRODUCT_ID NUMBER(10),
+    FOREIGN KEY (CUSTOMER_USERNAME) REFERENCES GPNR_CUSTOMER(CUSTOMER_USERNAME),
+    FOREIGN KEY (PRODUCT_ID) REFERENCES GPNR_PRODUCT(PRODUCT_ID)
 );
 
 CREATE TABLE gpnr_shipper (
-  shipper_username VARCHAR2(255) NOT NULL PRIMARY KEY,
-  shipper_password VARCHAR2(255) NOT NULL,
-  shipper_email VARCHAR2(255) NOT NULL,
-  shipper_firstname VARCHAR2(255) NOT NULL,
-  shipper_lastname VARCHAR2(255) NOT NULL,
-  shipper_phone VARCHAR2(20),
-  shipper_rating NUMBER(3, 2) NOT NULL DEFAULT 0
+    shipper_username VARCHAR2(255) NOT NULL PRIMARY KEY,
+    shipper_password VARCHAR2(255) NOT NULL,
+    shipper_email VARCHAR2(255) NOT NULL,
+    shipper_firstname VARCHAR2(255) NOT NULL,
+    shipper_lastname VARCHAR2(255) NOT NULL,
+    shipper_phone VARCHAR2(20),
+    shipper_rating DECIMAL(3, 2) NOT NULL DEFAULT 0
 );
 
-CREATE TABLE gpnr_shipping (
-  shipping_id NUMBER(10) NOT NULL PRIMARY KEY,
-  shipping_date TIMESTAMP(6) NOT NULL,
-  shipping_address CLOB,
-  shipping_status VARCHAR2(9) NOT NULL CHECK (shipping_status IN ('shipped', 'delivered')),
-  shipping_cost NUMBER(10, 2) NOT NULL,
-  shipping_rating NUMBER(3, 2) NOT NULL DEFAULT 0,
-  shipper_username VARCHAR2(255),
-  CONSTRAINT fk_shipper_username FOREIGN KEY (shipper_username) REFERENCES gpnr_shipper(shipper_username)
+CREATE TABLE GPNR_SHIPPING (
+    SHIPPING_ID NUMBER(10) NOT NULL PRIMARY KEY,
+    SHIPPING_DATE TIMESTAMP NOT NULL,
+    SHIPPING_ADDRESS CLOB,
+    SHIPPING_STATUS VARCHAR2(15) CHECK(SHIPPING_STATUS IN ('shipped', 'delivered')) NOT NULL DEFAULT 'shipped',
+    SHIPPING_COST DECIMAL(10, 2) NOT NULL,
+    SHIPPING_RATING DECIMAL(3, 2) NOT NULL DEFAULT 0,
+    SHIPPER_USERNAME VARCHAR2(255),
+    FOREIGN KEY (SHIPPER_USERNAME) REFERENCES GPNR_SHIPPER(SHIPPER_USERNAME)
 );
 
-CREATE TABLE gpnr_cart (
-  cart_id NUMBER(10) NOT NULL PRIMARY KEY,
-  cart_status VARCHAR2(10) NOT NULL CHECK (cart_status IN ('active', 'completed', 'cancelled')),
-  cart_created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
-  cart_promotion NUMBER(10, 2) NOT NULL DEFAULT 0,
-  total NUMBER(10, 2) NOT NULL,
-  tax NUMBER(10, 2) NOT NULL DEFAULT 0,
-  product_quantity NUMBER(10) NOT NULL DEFAULT 1,
-  customer_username VARCHAR2(255),
-  product_id NUMBER(10),
-  shipping_id NUMBER(10),
-  CONSTRAINT fk_customer_username_cart FOREIGN KEY (customer_username) REFERENCES gpnr_customer(customer_username),
-  CONSTRAINT fk_product_id_cart FOREIGN KEY (product_id) REFERENCES gpnr_product(product_id),
-  CONSTRAINT fk_shipping_id_cart FOREIGN KEY (shipping_id) REFERENCES gpnr_shipping(shipping_id)
+CREATE TABLE GPNR_CART (
+    CART_ID NUMBER(10) NOT NULL PRIMARY KEY,
+    CART_STATUS VARCHAR2(15) CHECK(CART_STATUS IN ('active', 'completed', 'cancelled')) NOT NULL DEFAULT 'active',
+    CART_CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CART_PROMOTION DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    TOTAL DECIMAL(10, 2) NOT NULL,
+    TAX DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    PRODUCT_QUANTITY NUMBER(10) NOT NULL DEFAULT 1,
+    CUSTOMER_USERNAME VARCHAR2(255),
+    PRODUCT_ID NUMBER(10),
+    SHIPPING_ID NUMBER(10),
+    FOREIGN KEY (CUSTOMER_USERNAME) REFERENCES GPNR_CUSTOMER(CUSTOMER_USERNAME),
+    FOREIGN KEY (PRODUCT_ID) REFERENCES GPNR_PRODUCT(PRODUCT_ID),
+    FOREIGN KEY (SHIPPING_ID) REFERENCES GPNR_SHIPPING(SHIPPING_ID)
 );
 
 CREATE TABLE gpnr_supplier (
-  supplier_username VARCHAR2(255) NOT NULL PRIMARY KEY,
-  supplier_password VARCHAR2(255) NOT NULL,
-  supplier_email VARCHAR2(255) NOT NULL,
-  supplier_firstname VARCHAR2(255) NOT NULL,
-  supplier_lastname VARCHAR2(255) NOT NULL,
-  supplier_phone VARCHAR2(20)
+    supplier_username VARCHAR2(255) NOT NULL PRIMARY KEY,
+    supplier_password VARCHAR2(255) NOT NULL,
+    supplier_email VARCHAR2(255) NOT NULL,
+    supplier_firstname VARCHAR2(255) NOT NULL,
+    supplier_lastname VARCHAR2(255) NOT NULL,
+    supplier_phone VARCHAR2(20)
 );
 
 CREATE TABLE gpnr_supplier_product (
-  supplier_username VARCHAR2(255),
-  product_id NUMBER(10),
-  PRIMARY KEY (supplier_username, product_id),
-  CONSTRAINT fk_supplier_product_id FOREIGN KEY (product_id) REFERENCES gpnr_product(product_id),
-  CONSTRAINT fk_supplier_username FOREIGN KEY (supplier_username) REFERENCES gpnr_supplier(supplier_username)
+    supplier_username VARCHAR2(255),
+    product_id NUMBER(10),
+    PRIMARY KEY (supplier_username, product_id),
+    FOREIGN KEY (product_id) REFERENCES gpnr_product(product_id),
+    FOREIGN KEY (supplier_username) REFERENCES gpnr_supplier(supplier_username)
 );
 
 CREATE TABLE gpnr_promotion (
-  promotion_id NUMBER(10) NOT NULL PRIMARY KEY,
-  promotion_name VARCHAR2(255) NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  amount NUMBER(10, 2) NOT NULL
+    promotion_id NUMBER(10) NOT NULL PRIMARY KEY,
+    promotion_name VARCHAR2(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE gpnr_customer_promotion (
-  promotion_id NUMBER(10),
-  customer_username VARCHAR2(255),
-  PRIMARY KEY (promotion_id, customer_username),
-  CONSTRAINT fk_customer_username_promotion FOREIGN KEY (customer_username) REFERENCES gpnr_customer(customer_username),
-  CONSTRAINT fk_promotion_id FOREIGN KEY (promotion_id) REFERENCES gpnr_promotion(promotion_id)
+    promotion_id NUMBER(10),
+    customer_username VARCHAR2(255),
+    PRIMARY KEY (promotion_id, customer_username),
+    FOREIGN KEY (customer_username) REFERENCES gpnr_customer(customer_username),
+    FOREIGN KEY (promotion_id) REFERENCES gpnr_promotion(promotion_id)
 );
