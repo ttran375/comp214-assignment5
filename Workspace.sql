f72ec43eb187644355dac9b06025ee85be84754e
T. Tran <ttran375@my.centennialcollege.ca>
2024-04-15 21:02:50 +0000

    Mon, Apr 15, 2024, 9:02 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,36 +1,15 @@
-CREATE OR REPLACE FUNCTION calculate_cart_total(cart_id_param IN NUMBER)
+CREATE OR REPLACE FUNCTION calculate_shipping_cost(
+    p_weight NUMBER
+)
 RETURN NUMBER
 IS
-    v_subtotal NUMBER(12, 2);
-    v_tax NUMBER(12, 2);
-    v_shipping_cost NUMBER(12, 2);
-    v_discount NUMBER(5, 2);
-    v_total NUMBER(12, 2);
+    lv_cost_per_kg NUMBER := 1.5;
+    lv_base_cost NUMBER := 5;
+    lv_shipping_cost NUMBER;
 BEGIN
-    SELECT SUM(p.price * pc.product_cart_quantity)
-    INTO v_subtotal
-    FROM gpnr_product p
-    JOIN gpnr_product_cart pc ON p.product_id = pc.product_id
-    WHERE pc.cart_id = cart_id_param;
+   
+    lv_shipping_cost := lv_base_cost + (p_weight * lv_cost_per_kg);
 
-    SELECT NVL(SUM(s.shipping_cost), 0)
-    INTO v_shipping_cost
-    FROM gpnr_shipping s
-    JOIN gpnr_cart c ON s.shipping_id = c.shipping_id
-    WHERE c.cart_id = cart_id_param;
-
-    SELECT NVL(SUM(c.cart_discount), 0)
-    INTO v_discount
-    FROM gpnr_cart c
-    WHERE c.cart_id = cart_id_param;
-
-    v_tax := 0.1 * v_subtotal;
-
-    v_total := v_subtotal + v_tax + v_shipping_cost - v_discount;
-
-    RETURN v_total;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RETURN 0; -- Return 0 if no data found
-END;
+    RETURN lv_shipping_cost;
+END calculate_shipping_cost;
 /
